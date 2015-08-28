87394a65b9ebdb4880f55b166a0da444544a97f5
ahs219 <ahs219@lehigh.edu>
2015-08-28 13:44:51 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,22 @@
+//Andrew Steinberg
+//Lab 01
+//Hello World Java Program
+
+//define class
+public class HelloWorld {
+//  main method
+    public static void main (String [] args) {
+        
+//  print statements
+
+    System.out.println("#########################");
+    System.out.println("#########################");
+    System.out.println("#                       #");
+    System.out.println("#    Hello World 2015   #");
+    System.out.println("#                       #");
+    System.out.println("#########################");
+    System.out.println("#########################");
+    System.out.println("test");
+    
+    }
+}
