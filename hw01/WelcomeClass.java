907f866e6e016a48e302722a9222d5077019423d
ahs219 <ahs219@lehigh.edu>
2015-09-02 00:24:20 +0000

    homework 1

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,16 @@
+//define class
+public class WelcomeClass {
+
+//main method
+public static void main (String[] args) {
+
+System.out.println("-----------");
+System.out.println("| WELCOME |");
+System.out.println("-----------");
+System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\");
+System.out.println("|-A--H--S--2--1--9-|");
+System.out.println("\\ /\\ /\\/\\ /\\ /\\ /");
+System.out.println("v  v  v  v  v  v");
+System.out.println("My favorite sport is basketball and my favorite team is the Boston Celtics");
+    }
+}
