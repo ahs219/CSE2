3e017765972bdea038e373725cb758156ad838ce
ahs219 <ahs219@lehigh.edu>
2015-09-25 14:05:04 +0000

    lab05 CalculatorSpecial

--- lab05/CalculatorSpecial.java
+++ lab05/CalculatorSpecial.java
@@ -1,12 +1,18 @@
-//Title: Lab 4 - Calculator.java
+//Title: Lab 5 - SpecialCalculator.java
 //Author: Andrew Steinberg 
 //Purpose: To create a program that creates a calculator that will ask for user input until they display "n" or "N"
 import java.util.Scanner;
-public class Calculator{
+public class CalculatorSpecial{
     public static void main (String[] args)
     {
         Scanner myScanner = new Scanner(System.in); // creates scanner so the user can input data
-        System.out.println("Please input an integer: "); //prompts the user to input the first integer
+        
+        System.out.println("Type n or N to stop"); //prompts user
+        String y = myScanner.next(); //accepts user input
+        
+        while (!y.equals("n") && !y.equals("N")) //while loop to run the calculator if they user doesnt input "n" or "N"
+        {
+             System.out.println("Please input an integer: "); //prompts the user to input the first integer
         int a = myScanner.nextInt();// accepts the user input
         
         System.out.println("Please input another integer: ");//prompts the user to input the second integer
@@ -28,7 +34,12 @@ public class Calculator{
         case '/': System.out.println((double) a / (double) b);//displays the fourth case if the user inputs a division sign
         break;// " " "
         
+        
         default: System.out.println("Invalid operator");// a default statement is needed to represent a case where the user doesnt input any of the four accepted operation signs
+        
+        }
+        System.out.println("Type n or N to stop"); //prompts the user again so there isnt an infinite loop
+        y = myScanner.next(); //accepts user input
         }
         
     }
